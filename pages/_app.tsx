7b4155ad30b9f7beea40cd28cb3851f09e5fde2e
KennedyNvsf <kennedy.freitas@marzeelabs.org>
2022-07-13 00:33:27 +0100

    app structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import {useState, useEffect }from "react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  const [isSSR, setisSSR] = useState(true);
+
+  useEffect(() => {
+    setisSSR(false)
+  }, [])
+  
+  if(isSSR) return null;
+
+  return (
+    <div className="app-container">
+      Navbar
+      <div className=".sidebar-container">Sidebar</div>
+      <div className="content-container"><Component {...pageProps} /></div>
+      
+    </div>
+  )
 }
 
 export default MyApp
